496cf5d6556fce13489e252d7dc1bd2fd3524d1a
Axyz7 <nautiyalaryan9406@gmail.com>
2026-03-24 18:00:16 +0530

    init: core VM architecture

---
+++ src/vm.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define MEMORY_SIZE 65536 //64kb RAM
+
+//opcodes from ISA spec V1
+#define OP_LOAD_VAL 0x01
+#define OP_LOAD_MEM 0x02
+#define OP_STORE_MEM 0x03
+#define OP_MOV_REG  0x04
+
+#define OP_ADD 0x10
+#define OP_SUB 0x11
+#define OP_CMP 0x12
+
+#define OP_JMP 0x20
+#define OP_JEQ 0x21
+#define OP_JNE 0x22
+
+#define OP_PUSH 0x30
+#define OP_POP  0x31
+
+#define OP_PRINT 0xF0
+#define OP_HALT  0xFF
+
+typedef struct{
+    uint8_t memory[MEMORY_SIZE];
+    uint16_t registers[4];
+    uint16_t pc;
+    uint16_t sp;
+    bool zf;
+    bool nf;
+    bool is_running;
+} VirtualMachine;
+
+void init_vm(VirtualMachine* vm){
+    for(int i=0;i<MEMORY_SIZE;i++) vm->memory[i]=0;
+    for(int i=0;i<4;i++) vm->registers[i]=0;
+    vm->pc=0x0000;
+    vm->sp=0xFFFF;
+    vm->zf=false;
+    vm->is_running=true;
+}
+
+
