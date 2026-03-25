182fe711e44d2914df8afd1a57a062677da6f58c
Axyz7 <nautiyalaryan9406@gmail.com>
2026-03-25 15:41:27 +0530

    feat: more operations and few comments

--- src/vm.c
+++ src/vm.c
@@ -10,18 +10,14 @@
 #define OP_LOAD_MEM 0x02
 #define OP_STORE_MEM 0x03
 #define OP_MOV_REG  0x04
-
 #define OP_ADD 0x10
 #define OP_SUB 0x11
 #define OP_CMP 0x12
-
 #define OP_JMP 0x20
 #define OP_JEQ 0x21
 #define OP_JNE 0x22
-
 #define OP_PUSH 0x30
 #define OP_POP  0x31
-
 #define OP_PRINT 0xF0
 #define OP_HALT  0xFF
 
@@ -30,8 +26,8 @@ typedef struct{
     uint16_t registers[4];
     uint16_t pc;
     uint16_t sp;
-    bool zf;
-    bool nf;
+    bool zf;  //zero flag
+    bool nf;  //negative flag
     bool is_running;
 } VirtualMachine;
 
@@ -39,21 +35,24 @@ void init_vm(VirtualMachine* vm){
     for(int i=0;i<MEMORY_SIZE;i++) vm->memory[i]=0;
     for(int i=0;i<4;i++) vm->registers[i]=0;
     vm->pc=0x0000;
-    vm->sp=0xFFFF;
+    vm->sp=0xFFFF;  // stack grows downwards
     vm->zf=false;
     vm->is_running=true;
 }
 
 uint8_t fetch_byte(VirtualMachine* vm){
-    uint8_t data=vm->memory[vm->pc];
-    vm->pc++;
-    return data;
+    return vm->memory[vm->pc++];
 }
 
 uint16_t fetch_word(VirtualMachine* vm){
     uint8_t low_byte=fetch_byte(vm);
     uint8_t high_byte=fetch_byte(vm);
-    return (high_byte<<8)|low_byte;
+    return (high_byte<<8)|low_byte;  //little-endian
+}
+
+void write_mem16(VirtualMachine* vm, uint16_t addr, uint16_t val){
+    vm->memory[addr]=val & 0xFF;   //low byte
+    vm->memory[addr+1]=(val>>8);   //high byte
 }
 
 void execute_instruction(VirtualMachine* vm, uint8_t opcode){
@@ -62,6 +61,24 @@ void execute_instruction(VirtualMachine* vm, uint8_t opcode){
             uint8_t reg=fetch_byte(vm);
             uint16_t val=fetch_word(vm);
             vm->registers[reg]=val;
+            break;
+        }
+        case OP_LOAD_MEM:{
+            uint8_t reg=fetch_byte(vm);
+            uint16_t addr=fetch_word(vm);
+            vm->registers[reg]=read_mem16(vm, addr);
+            break;
+        }
+        case OP_STORE_MEM:{
+            uint16_t addr=fetch_word(vm);
+            uint8_t reg=fetch_byte(vm);
+            write_mem16(vm,addr,vm->registers[reg]);
+            break;
+        }
+        case OP_MOV_REG:{
+            uint8_t regA=fetch_byte(vm);
+            uint8_t regB=fetch_byte(vm);
+            vm->registers[regA]=vm->registers[regB];
             break;
         }
         case OP_ADD:{
@@ -73,6 +90,15 @@ void execute_instruction(VirtualMachine* vm, uint8_t opcode){
 
             vm->zf=(result==0);
             vm->nf=(result & 0x8000)!=0; //check if highest bit is 1
+            break;
+        }
+        case OP_SUB:{
+            uint8_t regA=fetch_byte(vm);
+            uint8_t regB=fetch_byte(vm);
+            uint16_t res=vm->registers[regA]-vm->registers[regB];
+            vm->registers[regA]=res;
+            vm->zf=(res==0);
+            vm->nf=(res & 0x8000)!=0;
             break;
         }
         case OP_PRINT:{
@@ -101,4 +127,3 @@ void run_vm(VirtualMachine* vm){
     }
     printf("---VM Shut Down---\n");
 }
-
