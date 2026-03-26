1753b8d3cda5b0864d48b441ea4356035b1b6cc4
GigachadGuru <guruprasadbhandariofficial@gmail.com>
2026-03-26 20:06:44 +0530

    feat: add memory write and read function (#1)

---
+++ src/Memory.c
@@ -0,0 +1,57 @@
+#include <stdio.h>    // For printf (error messages)
+#include <stdint.h>   // For uint8_t and uint16_t
+#include <stdbool.h>  // For true/false
+#include <stdlib.h>   // For exit() if the VM crashes
+
+void write_memory(uint32_t address,uint8_t value);
+uint8_t read_memory(uint32_t address);
+# define MEMORY_SIZE 65536
+
+uint8_t Memory[MEMORY_SIZE];
+
+
+
+
+
+
+int main(void){
+    printf("-----VM--Memory--------\n");
+
+    uint16_t test_adr = 0x4321; //Decimal: 17185
+    uint8_t test_val = 0xAB; //Decimal: 171
+
+    printf("Writing 0x%X to the address 0x%04X\n",test_val,test_adr);
+    write_memory(test_adr,test_val);
+
+    uint8_t retrieved = read_memory(test_adr);
+    printf("retrieved value : 0x%X\n",retrieved);
+
+    if(retrieved == test_val){
+        printf("SUCCESS: Memory Integrity Verified!\n");
+    }
+    else{
+        printf("ERROR: Memory Mismatch!\n");
+    }
+    return 0;
+}
+
+void write_memory(uint32_t address,uint8_t value){
+    if(address>=MEMORY_SIZE){
+        printf("[FATAL ERROR]: Segmentation Fault! Attempted write at 0x%04X\n",address);
+        exit(1);
+    }
+    Memory[address] = value;
+}
+
+
+
+uint8_t read_memory(uint32_t address){
+    if(address>=MEMORY_SIZE){
+        printf("[FATAL ERROR]: Segmentation Fault! Attempted write at 0x%04X\n",address);
+        exit(1);
+    }
+    return Memory[address];
+}
+
+
+
