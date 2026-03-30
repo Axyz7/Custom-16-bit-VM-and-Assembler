7bb3fc5c94f54e62a36ee355f65b515f0d6711ab
GogachadGuru <guruprasadbhandariofficial@gmail.com>
2026-03-30 19:48:40 +0530

    feat: Implement memory stack/debug: clean vm.c from memory functions and ensure safety parameters,update struct parameters for safety purposes /

--- include/Memory.h
+++ include/Memory.h
@@ -7,5 +7,9 @@
 uint8_t read_memory(VirtualMachine *vm,uint32_t address);
 void write_memory(VirtualMachine *vm,uint32_t address,uint8_t value);
 
+void stack_push(VirtualMachine*vm, uint16_t value);
+uint16_t stack_pop(VirtualMachine*vm);
+void write_mem16(VirtualMachine *vm, uint16_t addr, uint16_t val) ;
+uint16_t read_mem16(VirtualMachine *vm, uint16_t addr);
 
 #endif
--- src/Memory.c
+++ src/Memory.c
@@ -1,5 +1,5 @@
 #include "../include/vm.h"
-
+#include "../include/Memory.h"
 #include <stdbool.h>  // For true/false
 #include <stdint.h>   // For uint8_t and uint16_t
 #include <stdio.h>    // For printf (error messages)
@@ -7,6 +7,8 @@
 
 void write_memory(VirtualMachine *vm, uint32_t address, uint8_t value);
 uint8_t read_memory(VirtualMachine *vm, uint32_t address);
+void write_mem16(VirtualMachine *vm, uint16_t addr, uint16_t val) ;
+uint16_t read_mem16(VirtualMachine *vm, uint16_t addr);
 
 void write_memory(VirtualMachine *vm, uint32_t address, uint8_t value) {
     if (address >= MEMORY_SIZE) {
@@ -22,4 +24,36 @@ uint8_t read_memory(VirtualMachine *vm, uint32_t address) {
         exit(1);
     }
     return vm->memory[address];
+}
+
+void stack_push(VirtualMachine*vm, uint16_t value){
+    uint32_t next_sp = vm->sp -2;
+    if(next_sp<400){
+        printf("[FATAL ERROR]: Stack Overflow");
+        exit(1);
+    }
+    vm->sp = next_sp;
+    write_mem16(vm,vm->sp,value);
+}
+
+uint16_t stack_pop(VirtualMachine*vm){
+   
+    if(vm->sp>=0xFFFD){
+        printf("[FATAL ERROR]: Stack Underflow");
+        exit(1);
+    }
+    uint16_t value= read_mem16(vm, vm->sp);
+    vm->sp +=2;
+    return value;
+}
+
+void write_mem16(VirtualMachine *vm, uint16_t addr, uint16_t val) {
+    vm->memory[addr] = val & 0xFF;      // low byte
+    vm->memory[addr + 1] = (val >> 8);  // high byte
+}
+
+uint16_t read_mem16(VirtualMachine *vm, uint16_t addr) {
+    uint8_t low = vm->memory[addr];
+    uint8_t high = vm->memory[addr + 1];
+    return (high << 8) | low;
 }
